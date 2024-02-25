dc15927c4ff07a873f93abee518e50d7cb9faccf
ASUS <ASUS@DESKTOP-BDU1F2V>
2024-02-25 20:25:04 +0400

    Proje dosyası ekle.

---
+++ Class_in_c#/Class_Point.cs
@@ -0,0 +1,54 @@
+﻿
+
+namespace Class_in_c_
+{
+    public class Class_Point
+    {
+
+        private int x;
+        public int x_
+        {
+            get { return x; }
+            set
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("Menfi eded daxil edile bilmez");
+                    return;
+                }
+                x = value;
+
+            }
+        }
+        private int y;
+        public int y_
+        {
+            get { return y; }
+            set
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("Menfi eded daxil edile bilmez");
+                    return;
+                }
+                y = value;
+
+            }
+        }
+
+        public void showdata()
+        {
+            Console.WriteLine($"x-->{x}, y-->{y}");
+        }
+        public Class_Point()
+        {
+            this.x = 0;
+            this.y = 0;
+        }
+        public Class_Point(int x,int y)
+        {
+            this.x_ = x;
+            this.y_ = y;
+        }
+    }
+}
---
+++ Class_in_c#/Program.cs
@@ -0,0 +1,6 @@
+﻿using Class_in_c_;
+
+
+Class_Point class_Point = new Class_Point(-1,2);
+
+class_Point.showdata();
